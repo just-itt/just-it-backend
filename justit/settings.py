17c7b936a16dae795426348c1f4a047b69c78dfa
2jisu <jisu.jjang88@gmail.com>
2023-03-18 18:06:33 +0900

    feat: add member model

--- justit/settings.py
+++ justit/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "members.apps.MembersConfig"
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- members/models.py
+++ members/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Member(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    nickname = models.CharField(max_length=128)
+    profile_image = models.FileField(upload_to="uploads/%Y/%m/%d", null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)

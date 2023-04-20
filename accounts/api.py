6f8d08e17cc427ad3a201f7879e6e71b07669504
2jisu <jisu.jjang88@gmail.com>
2023-04-20 16:59:08 +0900

    refactor: enum 사용 방식 수정

--- accounts/api.py
+++ accounts/api.py
@@ -28,7 +28,7 @@ router = Router()
 @router.post("/login", response={200: Token, 400: Error})
 def login(request, payload: Login):
     member = get_object_or_404(
-        Member, email=payload.email, status=MemberStatusEnum.ACTIVE
+        Member, email=payload.email, status=MemberStatusEnum.ACTIVE.value
     )
     if not check_password(payload.password, member.password):
         return 400, Message(message="Password is not correct")
@@ -64,7 +64,7 @@ def join_auth_send(request, payload: EmailAuthentication):
         auth_code=auth_code,
         expire_at=datetime.datetime.utcnow()
         + datetime.timedelta(minutes=EMAIL_AUTH_CODE_VALID_MINUTES),
-        type=EmailAuthenticationTypeEnum.JOIN,
+        type=EmailAuthenticationTypeEnum.JOIN.value,
     )
     EmailMessage(
         subject="[Just it] 회원 가입 인증 코드",
@@ -77,7 +77,7 @@ def join_auth_send(request, payload: EmailAuthentication):
 @router.post("/email-auth/check", response={200: Message, 400: Error})
 def join_auth_check(request, payload: EmailAuthenticationCode):
     email_auth = EmailAuth.objects.filter(
-        email=payload.email, type=EmailAuthenticationTypeEnum.JOIN
+        email=payload.email, type=EmailAuthenticationTypeEnum.JOIN.value
     ).last()
     utc_now = datetime.datetime.utcnow()
     if email_auth.auth_code != payload.auth_code:
@@ -85,23 +85,21 @@ def join_auth_check(request, payload: EmailAuthenticationCode):
     if email_auth.expire_at < utc_now:
         return 400, Message(message="Authentication code has expired")
     EmailAuth.objects.filter(
-        email=payload.email, type=EmailAuthenticationTypeEnum.JOIN
+        email=payload.email, type=EmailAuthenticationTypeEnum.JOIN.value
     ).all().delete()
     return Message(message="Success!")
 
 
 @router.post("/find-pw/send", response={200: Message, 400: Error})
 def find_password_auth_send(request, payload: EmailAuthentication):
-    get_object_or_404(
-        Member.objects.filter(email=payload.email, status=MemberStatusEnum.ACTIVE)
-    )
+    get_object_or_404(Member, email=payload.email, status=MemberStatusEnum.ACTIVE.value)
     auth_code = make_auth_code()
     EmailAuth.objects.create(
         email=payload.email,
         auth_code=auth_code,
         expire_at=datetime.datetime.utcnow()
         + datetime.timedelta(minutes=EMAIL_AUTH_CODE_VALID_MINUTES),
-        type=EmailAuthenticationTypeEnum.PASSWORD,
+        type=EmailAuthenticationTypeEnum.PASSWORD.value,
     )
     EmailMessage(
         subject="[Just it] 비밀 번호 찾기 인증 코드",
@@ -115,10 +113,12 @@ def find_password_auth_send(request, payload: EmailAuthentication):
 def find_password_auth_check(request, payload: EmailAuthenticationCode):
     email_auth = EmailAuth.objects.filter(
         email=payload.email,
-        type=EmailAuthenticationTypeEnum.PASSWORD,
-        expire_at__lte=datetime.datetime.utcnow(),
+        type=EmailAuthenticationTypeEnum.PASSWORD.value,
+        expire_at__gte=datetime.datetime.utcnow(),
         is_auth=False,
     ).last()
+    if not email_auth:
+        return 400, {"message": "Email authentication has not been completed"}
     if email_auth.auth_code != payload.auth_code:
         return 400, Message(message="Authentication code is not valid")
     email_auth.is_auth = True
@@ -130,18 +130,18 @@ def find_password_auth_check(request, payload: EmailAuthenticationCode):
 def reset_password(request, payload: Login):
     email_auth = EmailAuth.objects.filter(
         email=payload.email,
-        type=EmailAuthenticationTypeEnum.PASSWORD,
+        type=EmailAuthenticationTypeEnum.PASSWORD.value,
         is_auth=True,
-        expire_at__lte=datetime.datetime.utcnow(),
+        expire_at__gte=datetime.datetime.utcnow(),
     )
     if not email_auth:
         return 400, {"message": "Email authentication has not been completed"}
     member = get_object_or_404(
-        Member, email=payload.email, status=MemberStatusEnum.ACTIVE
+        Member, email=payload.email, status=MemberStatusEnum.ACTIVE.value
     )
     member.password = make_password(payload.password)
     member.save()
     EmailAuth.objects.filter(
-        email=payload.email, type=EmailAuthenticationTypeEnum.PASSWORD
+        email=payload.email, type=EmailAuthenticationTypeEnum.PASSWORD.value
     ).all().delete()
     return Message(message="Success!")

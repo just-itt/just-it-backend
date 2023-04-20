a30ef536c922a07a1855b7d7450f663d0da33bdf
2jisu <jisu.jjang88@gmail.com>
2023-04-20 16:58:41 +0900

    feat: 사용자 프로필 이미지 추가/제거 API 생성

--- members/api.py
+++ members/api.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404
-from ninja import Router
+from ninja import Router, UploadedFile, File
 from ninja.responses import codes_4xx
 
 from accounts.schema import (
     AuthBearer,
 )
 from common.schema import Error, Message
+from common.utils.s3_upload import S3ImgUploader
 from members.schema import MemberOut, MemberIn, MemberStatusEnum
 from members.models import Member
 
@@ -39,7 +40,38 @@ def update_member(request, payload: MemberIn):
 def withdraw(request):
     if request.auth == 401:
         return 401, {"message": "Unauthorized"}
-    member = get_object_or_404(Member, id=request.auth.get("id"))
-    member.status = MemberStatusEnum.WITHDRAW
+    member = get_object_or_404(
+        Member, id=request.auth.get("id"), status=MemberStatusEnum.ACTIVE.value
+    )
+    member.status = MemberStatusEnum.WITHDRAW.value
     member.save()
     return 200, {"message": "Success!"}
+
+
+@router.post("/me/profile-image", response={200: MemberOut, codes_4xx: Error})
+def post_member_profile_image(request, file: UploadedFile = File(...)):
+    if request.auth == 401:
+        return 401, {"message": "Unauthorized"}
+    member = get_object_or_404(
+        Member, id=request.auth.get("id"), status=MemberStatusEnum.ACTIVE.value
+    )
+    if not member.profile_image:
+        member.profile_image = S3ImgUploader().upload(
+            file=file, domain=f"images/members/{member.id}"
+        )
+        member.save()
+    return member
+
+
+@router.delete("/me/profile-image", response={200: MemberOut, codes_4xx: Error})
+def delete_member_profile_image(request):
+    if request.auth == 401:
+        return 401, {"message": "Unauthorized"}
+    member = get_object_or_404(
+        Member, id=request.auth.get("id"), status=MemberStatusEnum.ACTIVE.value
+    )
+    if member.profile_image:
+        if S3ImgUploader().delete(member.profile_image):
+            member.profile_image = None
+            member.save()
+    return member

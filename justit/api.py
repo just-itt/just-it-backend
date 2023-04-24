9fa173aac4d725a7253aca37e9f87ec785f56f15
2jisu <jisu.jjang88@gmail.com>
2023-04-24 16:21:59 +0900

    feat: post, tag router 추가

--- justit/api.py
+++ justit/api.py
@@ -1,8 +1,12 @@
 from ninja import NinjaAPI
 from accounts.api import router as auth_router
 from members.api import router as member_router
+from posts.api import router as post_router
+from tags.api import router as tag_router
 
 api_v1 = NinjaAPI(title="Just-it Backend", version="1.0")
 
-api_v1.add_router("/accounts/", auth_router, tags=["Account"])
-api_v1.add_router("/members/", member_router, tags=["Member"])
+api_v1.add_router("/accounts", auth_router, tags=["Account"])
+api_v1.add_router("/members", member_router, tags=["Member"])
+api_v1.add_router("/posts", post_router, tags=["Post"])
+api_v1.add_router("/tags", tag_router, tags=["Tag"])

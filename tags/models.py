805823b77360b417f9bc9ac834101747bb919dab
2jisu <jisu.jjang88@gmail.com>
2023-04-24 16:20:23 +0900

    feat: tag 관련 모델 생성

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=64)
+    is_required = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class Option(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)

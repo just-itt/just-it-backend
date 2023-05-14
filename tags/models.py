04ff739989245b77cf6fbc21dbadd3f07ca56d05
2jisu <jisu.jjang88@gmail.com>
2023-05-14 17:58:19 +0900

    feat: 맞춤 필터 모델 추가

--- tags/models.py
+++ tags/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from members.models import Member
+
 
 class Tag(models.Model):
     title = models.CharField(max_length=64)
@@ -11,5 +13,13 @@ class Tag(models.Model):
 class Option(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class Customization(models.Model):
+    title = models.CharField(max_length=64)
+    tag_options = models.ManyToManyField(Option)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)

8702ef2f8c5e477e61d5b0d396c25f77b4588aca
2jisu <jisu.jjang88@gmail.com>
2023-05-14 17:58:49 +0900

    feat: 맞춤 필터 등록/조회 API 추가

--- tags/api.py
+++ tags/api.py
@@ -6,8 +6,8 @@ from accounts.schema import (
     AuthBearer,
 )
 from common.schema import Error
-from tags.models import Tag
-from tags.schema import TagOutWithOptions
+from tags.models import Tag, Customization
+from tags.schema import TagOutWithOptions, CustomizationTagIn, CustomizationTagOut
 
 router = Router(auth=AuthBearer())
 
@@ -17,3 +17,27 @@ def get_tags(request):
     if request.auth == 401:
         return 401, Error(detail="Unauthorized")
     return Tag.objects.prefetch_related("option_set")
+
+
+@router.get("/me", response={200: CustomizationTagOut, 401: Error})
+def get_customization_tags(request):
+    if request.auth == 401:
+        return 401, Error(detail="Unauthorized")
+    custom_tag = Customization.objects.get(member_id=request.auth.get("id"))
+    return custom_tag
+
+
+@router.post("/me", response={200: CustomizationTagOut, 401: Error})
+def create_customization_tags(request, payload: CustomizationTagIn):
+    if request.auth == 401:
+        return 401, Error(detail="Unauthorized")
+    if Customization.objects.filter(member_id=request.auth.get("id")).exists():
+        custom_tag = Customization.objects.get(member_id=request.auth.get("id"))
+        custom_tag.title = payload.title
+        custom_tag.tag_options.set(payload.tag_options)
+    else:
+        custom_tag = Customization.objects.create(
+            title=payload.title, member_id=request.auth.get("id")
+        )
+        custom_tag.tag_options.set(payload.tag_options)
+    return custom_tag
--- tags/schema.py
+++ tags/schema.py
@@ -31,3 +31,16 @@ class TagOut(Schema):
 
 class TagOutWithOptions(TagOut):
     options: List[TagOptionOut] = Field(None, alias="option_set")
+
+
+class CustomizationTagIn(Schema):
+    title: str
+    tag_options: List[int]
+
+
+class CustomizationTagOut(Schema):
+    id: int
+    title: str
+    tag_options: List[TagOptionOut] = None
+    created_at: datetime
+    updated_at: Optional[datetime]

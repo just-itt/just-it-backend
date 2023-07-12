fa9504f8c7509a1e7f3d484d12577569ee3bd070
2jisu <jisu.jjang88@gmail.com>
2023-07-12 18:16:33 +0900

    fix: custom auth 수정

--- common/utils/auth.py
+++ common/utils/auth.py
@@ -4,5 +4,6 @@ from accounts.schema import AuthBearer
 def custom_auth(request):
     token = request.headers.get("Authorization")
     if token:
+        token = token.split("Bearer")[1].strip()
         return AuthBearer().authenticate(request=request, token=token)
     return 401
